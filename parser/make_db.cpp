e4ecb4185f7690ae837d009f82dc084df912c770
gopuvenkat <Gopalakrishnan.V@iiitb.org>
2020-04-08 12:16:07 +0530

    initial commit: table created

--- parser/make_db.cpp
+++ parser/make_db.cpp
@@ -1,15 +1,18 @@
-// #include "../libs/pugixml-1.10/src/pugixml.hpp"
+#include "../libs/pugixml-1.10/src/pugixml.hpp"
 #include <iostream>
 #include <pqxx/pqxx>
+#include <string.h>
 
 using namespace std;
-using namespace pqxx;
+
+void create_db(pqxx::connection &c);
 
 int main() {
     try {
         pqxx::connection c("dbname = column_store user = test password = test hostaddr = 127.0.0.1 port = 5432");
         if(c.is_open()) {
             cout << "Opened the database successfully: " << c.dbname() << endl;
+            create_db(c);
         }
         else {
             cout << "Can't open the database" << endl;
@@ -21,4 +24,13 @@ int main() {
         return 1;
     }
     return 0;
+}
+
+void create_db(pqxx::connection &c) {
+    string table_name = "test";
+    string sql = "CREATE TABLE " + table_name + " (ID INT PRIMARY KEY NOT NULL);";
+    pqxx::work W(c);
+    W.exec(sql);
+    W.commit();
+    cout << "Table " + table_name + " created successfully" << endl;
 }
