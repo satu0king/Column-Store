fd4d134a5ede4311387793f91eff53f59c2e8b74
ThisWasntTaken <ananthshreekumar@gmail.com>
2020-04-08 13:31:26 +0530

    Initial commit parse XML

--- parser/make_db.cpp
+++ parser/make_db.cpp
@@ -5,19 +5,22 @@
 
 using namespace std;
 
-void create_db(pqxx::connection &c);
+void create_table(pqxx::connection &c);
+void parse_xml(const char* file_path);
 
 int main() {
     try {
         pqxx::connection c("dbname = column_store user = test password = test hostaddr = 127.0.0.1 port = 5432");
         if(c.is_open()) {
             cout << "Opened the database successfully: " << c.dbname() << endl;
-            create_db(c);
         }
         else {
             cout << "Can't open the database" << endl;
             return 1;
         }
+
+        parse_xml("../schema.xml");
+
         c.disconnect();
     } catch (const std::exception &e) {
         cerr << e.what() << std::endl;
@@ -26,7 +29,22 @@ int main() {
     return 0;
 }
 
-void create_db(pqxx::connection &c) {
+void parse_xml(const char* file_path) {
+    pugi::xml_document doc;
+    pugi::xml_parse_result result = doc.load_file(file_path);
+    if (!result) {
+        throw "XML file not found.";
+    }
+    
+    pugi::xpath_node_set tables = doc.select_nodes("/schema/tables/table/table_name");
+
+    for (pugi::xpath_node table : tables) {
+        pugi::xml_node tool = table.node();
+        cout << tool.child_value() << endl;
+    }
+}
+
+void create_table(pqxx::connection &c) {
     string table_name = "test";
     string sql = "CREATE TABLE " + table_name + " (ID INT PRIMARY KEY NOT NULL);";
     pqxx::work W(c);
