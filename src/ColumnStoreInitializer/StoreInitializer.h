53161372e2c141153f5fb8322c5e7af3f6047d6c
Satvik Ramaprasad <satviksr@gmail.com>
2020-04-27 13:59:35 +0530

    FileSystem fix

--- src/ColumnStoreInitializer/StoreInitializer.h
+++ src/ColumnStoreInitializer/StoreInitializer.h
@@ -6,7 +6,15 @@
 
 #include "Parser/SchemaMetaData.h"
 
-namespace fs = std::experimental::filesystem::v1;
+#ifdef __cpp_lib_filesystem
+    #include <filesystem>
+    namespace fs = std::filesystem;
+#elif __cpp_lib_experimental_filesystem
+    #include <experimental/filesystem>
+    namespace fs = std::experimental::filesystem;
+#else
+    #error "no filesystem support ='("
+#endif
 
 using nlohmann::json;
 
